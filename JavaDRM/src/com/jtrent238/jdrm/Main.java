a3884442a1094ac82d915bf7106f05025e7b2850
unknown <jtrent238@outlook.com>
2020-12-31 21:46:47 -0500

    12312020946

---
+++ JavaDRM/src/com/jtrent238/jdrm/Main.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.jtrent238.jdrm;
+
+
+public class Main {
+
+	/**
+	 * 
+	 */
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
